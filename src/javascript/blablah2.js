78fbe0134bdb03bc3e8acdb3787afb3fe8e325dc
Joshua Hale <josh@semmle.com>
2018-06-21 11:00:38 +0100

    add blahblah2.js without an alert

---
+++ src/javascript/blablah2.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// Start of generated code
+
+function CAIELmRxen() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function gFyhQjsXuP() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
