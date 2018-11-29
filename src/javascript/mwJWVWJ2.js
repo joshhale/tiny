49dbc2cf1fe5388fe3bd33d3be95cef7409a8046
Joshua Hale <josh@semmle.com>
2018-11-29 14:59:48 +0000

    add alert

---
+++ src/javascript/mwJWVWJ2.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+// Start of generated code
+
+function CAIELmRxen() {
+    var x = "";
+}
+// End of generated code
+
+// Start of generated code
+
+function gFyhQjsXuP() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
